707eff8d9b9ccee3bbcda266dbcc254c763043f3
Zoë Davidson <zoe.a.h.davidson@gmail.com>
2021-07-02 16:43:26 -0400

    encyclo complete

--- views/layout.js
+++ views/layout.js
@@ -4,14 +4,22 @@ module.exports = (content) => html`<!DOCTYPE html>
   <html lang="en">
     <head>
       <title>WikiStack</title>
-      <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
-      <link href="/stylesheets/style.css" rel="stylesheet">
+      <link
+        href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css"
+        rel="stylesheet"
+      />
+      <link href="/stylesheets/style.css" rel="stylesheet" />
     </head>
     <body>
       <div class="navbar navbar-fixed-top navbar-inverse" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nav-items">
+            <button
+              type="button"
+              class="navbar-toggle"
+              data-toggle="collapse"
+              data-target="#nav-items"
+            >
               <span class="sr-only">Toggle navigation</span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
@@ -28,12 +36,8 @@ module.exports = (content) => html`<!DOCTYPE html>
           </div>
         </div>
       </div>
-      <div class="container content">
-        $${content}
-      </div>
-      <hr/>
-      <div id="footer" class="container text-muted">
-        WIKISTACK by Fullstack Academy
-      </div>
+      <div class="container content">$${content}</div>
+      <hr />
+      <div id="footer" class="container text-muted">ENCYCLO</div>
     </body>
   </html>`;
